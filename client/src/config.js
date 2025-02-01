e246cf6d7b2cc9c13656924e76e5633b163d1bfd
11-vector <105460224+11-vector@users.noreply.github.com>
2025-02-01 14:16:17 +0000

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = https://pocproject.onrender.com
+export const API_URL = 'https://pocproject.onrender.com'
 //export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000'; 
